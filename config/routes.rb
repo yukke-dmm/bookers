303cf214c26f78fbe3f1448dec9ea3f87e1d9aa2
森田悠介 <yusuke@moritayuusukenoMacBook-Air.local>
2020-06-05 13:39:13 +0900

    Homeコントローラにし忘れた。まぁいいか。トップページとアバウトできた。

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#top'
+  get 'tops/about' => 'tops#about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:index, :show, :edit]
+  
 end
